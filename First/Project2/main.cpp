626d575707374524c9e208676dee1b909f93f886
Jake Horner <jakehorner19@gmail.com>
2022-02-16 13:54:52 +0000

    more practice

---
+++ First/Project2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+	int num1;
+	
+	std::cout << "Enter your favourite number: " <<std::endl;
+	std::cin >> num1;
+	std::cout << "Wow!... " << num1 <<std::endl;
+	//std::cout << num1  <<std::endl;
+	return 0;
+}
