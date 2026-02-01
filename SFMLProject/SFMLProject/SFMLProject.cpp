ce5203f7cb651bf968a230dbd1abe920999e9e3d
Jake Horner <jakehorner19@gmail.com>
2026-02-01 16:05:49 +0000

    commit

--- SFMLProject/SFMLProject/SFMLProject.cpp
+++ SFMLProject/SFMLProject/SFMLProject.cpp
@@ -6,5 +6,39 @@ int Text();
 int main()
 {
     //ScreenBounce();
-    Text();
+    /*Text();*/
+
+	//init window
+	sf::RenderWindow window(sf::VideoMode(1280, 720), "Game 1", sf::Style::Titlebar | sf::Style::Close);
+
+	//event
+	sf::Event ev;
+
+	//Game loop
+	while (window.isOpen())
+	{
+		//Event polling
+		while (window.pollEvent(ev))
+		{
+			switch (ev.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::KeyPressed:
+				if (ev.key.code == sf::Keyboard::Escape)
+					window.close();
+				break;
+			}
+		}
+
+		//Update
+
+
+		//Render
+		window.clear();
+		window.display();
+	}
+
+	return 0;
 }
